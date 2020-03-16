929a11f34d3f449a2413dc459f10e9ad4a50f13c
Gordiig <gordiig@gmail.com>
2020-03-16 18:29:27 +0300

    Added models

--- Places/models.py
+++ Places/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    """
+    Модель места
+    """
+    name = models.CharField(max_length=512, null=False, blank=False)
+    latitude = models.FloatField(null=False, blank=False)
+    longitude = models.FloatField(null=False, blank=False)
+    address = models.CharField(max_length=512, null=False, blank=False)
+    checked_by_moderator = models.BooleanField(default=False)
+    created_by = models.PositiveIntegerField(null=False, blank=False)
+    created_dt = models.DateTimeField(auto_now_add=True)
+    deleted_flg = models.BooleanField(default=False)
+
+    @property
+    def rating(self) -> float:
+        ratings_vals = [x.rating for x in self.ratings]
+        return sum(ratings_vals) / len(ratings_vals) if len(ratings_vals) != 0 else 0.0
+
+    def __str__(self):
+        return f'Place {self.name}'
+
+
+class Accept(models.Model):
+    """
+    Модель для одобрения выложенного места
+    """
+    created_by = models.PositiveIntegerField(null=False, blank=False)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='accepts')
+    created_dt = models.DateTimeField(auto_now_add=True)
+    deleted_flg = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Accept({self.id}) by {self.created_by}, on place {self.place.id}'
+
+
+class Rating(models.Model):
+    """
+    Модель рейтинга, который юзер дает месту
+    """
+    created_by = models.PositiveIntegerField(null=False, blank=False)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='ratings')
+    rating = models.PositiveIntegerField(null=False, blank=False)
+    created_dt = models.DateTimeField(auto_now_add=True)
+    updated_dt = models.DateTimeField(auto_now_add=True)
+    deleted_flg = models.BooleanField(default=False)
+
+
+class PlaceImage(models.Model):
+    """
+    Модель картинки места
+    """
+    created_by = models.PositiveIntegerField(null=False, blank=False)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
+    pic_link = models.URLField(null=False, blank=False)
+    created_dt = models.DateTimeField(auto_now_add=True)
+    deleted_flg = models.BooleanField(default=False)
