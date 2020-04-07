e70d53ae84f30f177c8d7d0ec53560f1d90c88b1
Gordiig <gordiig@gmail.com>
2020-04-07 12:30:45 +0300

    delete submodule

--- ApiRequesters/Auth/AuthRequester.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from ._AuthRequester import AuthRequester as __a, MockAuthRequester as __m
-
-__tst = settings.TESTING
-try:
-    __ar = settings.ALLOW_REQUESTS
-    AuthRequester = __a if __ar and not __tst else __m
-except AttributeError:
-    AuthRequester = __a if not __tst else __m
